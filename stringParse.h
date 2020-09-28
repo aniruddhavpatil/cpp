5a8b7423bc6484bc57ce87fd32626bfa5253f640
aniruddhavpatil <aniruddhavivekpatil@gmail.com>
2020-09-28 14:58:59 -0400

    Shortened stringParse

--- stringParse.h
+++ stringParse.h
@@ -3,20 +3,14 @@ using namespace std;
 #ifndef STRING_PARSE_H
 #define STRING_PARSE_H
 
-vector<string> stringParse(string s, string d)
-{
+vector<string> stringParse(string s, string d){
   vector<string> ans;
-  size_t curr = 0, next;
-  string token;
-
-  do
-  {
+  int curr = 0, next;
+  do{
     next = s.find(d, curr);
-    token = s.substr(curr, next - curr);
-    curr = next + d.length();
-    ans.push_back(token);
-  } while (next != string::npos);
-
+    ans.push_back(s.substr(curr, next-curr));
+    curr = next + d.size();
+  } while(next != string::npos);
   return ans;
 }
 

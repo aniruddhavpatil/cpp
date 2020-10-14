250128276f984a10417a891f89153f04fb296af2
aniruddhavpatil <aniruddhavivekpatil@gmail.com>
2020-10-14 16:32:07 -0400

    Added an alternate solution with a struct

--- labyrinth.cpp
+++ labyrinth.cpp
@@ -1,12 +1,54 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool isEscapePossible(
+struct Cell {
+    char type;
+    int y;
+    int x;
+};
+
+bool isEscapePossibleStruct(
     int n,
     int m,
     vector<vector<int>> obstacles,
     vector<vector<int>> teleport
     ){
+        vector<vector<Cell>> grid(n, vector<Cell>(m, {'.', -1, -1}));
+        for(auto obs : obstacles){
+            grid[obs[0]][obs[1]].type = 'o';
+        }
+
+        for(auto tp : teleport){
+            grid[tp[0]][tp[1]] = {'t', tp[2], tp[3]};
+        }
+
+        int i = 0, j = 0;
+        while(!(i == n-1 && j == m-1)){
+            Cell* curr = &grid[i][j];
+            cout << i << ' ' << j << ' ' << curr->type << endl;
+            if(curr->type == 'o' || curr->type == 'x') return false;
+            else if (curr->type == 't'){
+                curr->type = 'x';
+                i = curr->y;
+                j = curr->x;
+            }
+            else if (curr->type == '.'){
+                curr->type = 'x';
+                if(j < m - 1){
+                    j++;
+                }
+            }
+        }
+        return true;
+
+    }
+
+    bool isEscapePossible(
+        int n,
+        int m,
+        vector<vector<int>> obstacles,
+        vector<vector<int>> teleport)
+{
     vector<vector<pair<char, pair<int, int>>>> grid(n, vector<pair<char, pair<int, int>>>(m, {'.', {-1, -1}}));
     cout<<"OBS\n";
     for(auto obs: obstacles){
@@ -41,13 +83,12 @@ bool isEscapePossible(
     return true;
 }
 
-
 int main(){
 
     int n = 4, m = 4;
     vector<vector<int>> obastacle;
     vector<vector<int>> teleport = {{0,2,3,1}};
-    bool escape = isEscapePossible(n, m, obastacle, teleport);
+    bool escape = isEscapePossibleStruct(n, m, obastacle, teleport);
     if(escape){
         cout<<"Escape is possible\n";
     }

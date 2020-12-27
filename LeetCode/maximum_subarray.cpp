301f3d8ebe0f95ddb22d566e41f60f0bda2fb8a0
aniruddhavpatil <aniruddhavivekpatil@gmail.com>
2020-12-27 10:51:32 -0500

    Added maximum_subarray.cpp

---
+++ LeetCode/maximum_subarray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        vector<int> dp(nums.size(), INT_MIN);
+        dp[0] = nums[0];
+        int ans = dp[0];
+        for(int i = 1; i < nums.size(); i++){
+            dp[i] = nums[i] + (dp[i-1] > 0 ? dp[i-1] : 0);
+            ans = max(ans, dp[i]);
+        }
+        return ans;
+        
+    }
+};
